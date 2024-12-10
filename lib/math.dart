622fc27387ac2b38c6e3e246135ecfb564f69c1b
ilcanez <124120567+ilcanez@users.noreply.github.com>
2024-12-10 12:50:05 +0700

    Create math.dart
    Kelas untuk membuat random kode bukti transaksi

---
+++ lib/math.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+String generateTransactionID({int length = 8}) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  final random = Random();
+  return String.fromCharCodes(
+    Iterable.generate(
+      length,
+      (_) => characters.codeUnitAt(random.nextInt(characters.length)),
+    ),
+  );
+}
